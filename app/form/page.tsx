302462fadc2006f814636e2f62c345a93f797312
yfitjapan <fujiiyousay@gmail.com>
2025-08-18 20:07:51 +0900

    0818debug

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -7,11 +7,11 @@ export default function FormPage() {
       <Header />
       <main className="container mx-auto px-4 py-24">
         <h1 className="text-4xl font-bold text-center mb-8">申し込みフォーム</h1>
-        <div className="aspect-w-16 aspect-h-9">
+        <div>
           <iframe
             src="https://docs.google.com/forms/d/e/1FAIpQLSeK6IjTYXoYdUfRbpAmDxg-rgJxCEbpLySTAnsTqXXH_At5TA/viewform?usp=sharing&ouid=106111889620545617242"
             width="100%"
-            height="100%"
+            height="1200"
             frameBorder="0"
             marginHeight={0}
             marginWidth={0}
